c4431a1f78ae2ee1d7881b05fbd0ae388103a7f2
cwelstead <welstead.cole@gmail.com>
2025-02-17 12:33:26 -0600

    Saved changes from tutorial in Home.jsx

--- live-cursors-app/client/src/Home.jsx
+++ live-cursors-app/client/src/Home.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react'
+import throttle from 'lodash.throttle'
 import useWebSocket from 'react-use-websocket'
 
 export function Home({ username }) {
@@ -6,6 +8,18 @@ export function Home({ username }) {
         share: true,
         queryParams: { username }
     })
+
+    const THROTTLE = 50
+    const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE))
+
+    useEffect(() => {
+        window.addEventListener("mousemove", e=> {
+            sendJsonMessageThrottled.current({
+                x: e.clientX,
+                y: e.clientY
+            })
+        })
+    }, [])
 
     return <h1>Hello, {username}</h1>
 }
