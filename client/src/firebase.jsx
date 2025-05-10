a124b33873165228cf37cc9f6e82ed8d20082444
DanielKasparek <danielkasparek04@gmail.com>
2025-05-10 17:47:50 -0500

    use variable for apiKey

--- client/src/firebase.jsx
+++ client/src/firebase.jsx
@@ -4,7 +4,7 @@ import { getFirestore, doc, getDoc, setDoc, updateDoc } from "firebase/firestore
 
 
 const app = initializeApp({
-  apiKey: "AIzaSyBopB6HB2TkRuiydzYIA1QT9nJeFNsy1wI",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
